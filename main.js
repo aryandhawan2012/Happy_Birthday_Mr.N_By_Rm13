e292aa2eb190f7bd3d5e012270db303db606cebc
aryandhawan2012 <83532570+aryandhawan2012@users.noreply.github.com>
2023-09-24 17:47:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+function preload() {
+    window.location = "index.html";
+}
+
+function wrong1() {
+    window.location = "wrong1.html";
+}
+
+function correct1() {
+    window.location = "correct1.html";
+}
+
+function correct2() {
+    window.location = "correct2.html";
+}
+
+function wrong2() {
+    window.location = "wrong2.html";
+}
+
+function correct3() {
+    window.location = "correct3.html";
+}
+
+function wrong3() {
+    window.location = "wrong3.html";
+}
+
+function correct4() {
+    window.location = "correct4.html";
+}
+
+function wrong4() {
+    window.location = "wrong4.html";
+}
+
+function correct5() {
+    window.location = "correct5.html";
+}
+
+function wrong5() {
+    window.location = "wrong5.html";
+}
+
+function correct6() {
+    window.location = "correct6.html";
+}
+
+function wrong6() {
+    window.location = "wrong6.html";
+}
+
+function startquiz() {
+    window.location = "quiz1.html";
+}
+
+function onetwonext() {
+    window.location = "quiz2.html";
+}
+
+function twothreenext() {
+    window.location = "quiz3.html";
+}
+
+function threefournext() {
+    window.location = "quiz4.html";
+}
+
+function fourfivenext() {
+    window.location = "quiz5.html";
+}
+
+function fivesixnext() {
+    window.location = "quiz6.html";
+}
+
+function goback() {
+    window.location = "quiz.html";
+}
